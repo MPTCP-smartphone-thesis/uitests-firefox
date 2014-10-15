6bccaa69f03a8e444b783129ed6a205bb7a9926d
Quentin De Coninck <quentin.deconinck@student.uclouvain.be>
2014-10-15 11:51:27 +0200

    [ADD] UI test for Firefox

---
+++ src/firefox/LaunchSettings.java
@@ -0,0 +1,38 @@
+package firefox;
+
+import utils.Utils;
+
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class LaunchSettings extends UiAutomatorTestCase {
+
+	private static final String URL_BAR_ID = "org.mozilla.firefox_beta:id/url_bar_title";
+	private static final String URL_EDIT_ID = "org.mozilla.firefox_beta:id/url_edit_text";
+
+	private void visitWebsite(String url) {
+		Utils.click(URL_BAR_ID);
+		Utils.setText(URL_EDIT_ID, url);
+		getUiDevice().pressEnter();
+	}
+
+	public void testDemo() throws UiObjectNotFoundException {
+		assertTrue("OOOOOpps",
+				Utils.openApp(this, "Firefox Beta", "org.mozilla.firefox_beta"));
+
+		sleep(3000);
+
+		visitWebsite("http://multpath-tcp.org");
+		sleep(20000);
+		visitWebsite("https://www.flickr.com/");
+		sleep(20000);
+		visitWebsite("http://www.nytimes.com/");
+		sleep(20000);
+		visitWebsite("http://www.rtl.be/info");
+		sleep(20000);
+		visitWebsite("http://www.rtbf.be/");
+		sleep(20000);
+		visitWebsite("http://www.alexa.com/topsites");
+	}
+
+}
